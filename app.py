f4642afcde6d489b21b4e075d488e7f26c270683
Romina <guerreiromina@gmail.com>
2024-07-02 13:47:17 -0300

    conexion con la base de datos

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request,redirect, send_from_directory
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'gottravel'
+### Puerto MySQL en Xampp
+app.config['MYSQL_PORT'] = 3307
+
+mysql = MySQL(app)
 
 @app.route('/')
 def index():
   return render_template('index.html')
 
-@app.route('/destinsos')
+@app.route('/destinos')
 def destinos():
   return render_template('destinations.html')
 
@@ -25,7 +35,17 @@ def validacionform():
 
 @app.route('/viajes')
 def viajes():
-  return render_template('viajes/viajes.html')
+  sql = "SELECT * FROM viajes;"
+  
+  conn = mysql.connection
+  cursor = conn.cursor() 
+  cursor.execute(sql)
+  
+  db_viajes = cursor.fetchall()
+  
+  cursor.close()
+  
+  return render_template('viajes/viajes.html', viajes=db_viajes)
 
 if __name__=='__main__':
   app.run(debug=True)
